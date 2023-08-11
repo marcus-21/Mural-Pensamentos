a46f863a484c4944b816e6d3fbeadc62e9b6f0bf
Marcus silva <marvmms@gmail.com>
2023-08-11 16:30:37 -0300

    fix: validade pattern and bottom save

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -9,24 +10,48 @@ import { Router } from '@angular/router';
   styleUrls: ['./criar-pensamento.component.css']
 })
 export class CriarPensamentoComponent {
-  pensamento: Pensamento = {
 
-    conteudo: '',
-    autoria: '',
-    modelo:'Modelo 1'
-  }
+  
+  formulario!: FormGroup;
+
 
   constructor(
     private service: PensamentoService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) { }
 
-  criarPensamento() {
-    this.service.criar(this.pensamento).subscribe(()=>{
-      this.router.navigate(['/listarPensamento'])
+  ngOnInit():void {
+    this.formulario = this.formBuilder.group({
+      conteudo: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
+      ])],
+      autoria: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3)
+      ])],
+      modelo: ['modelo 1']
     })
+  }
+
+  criarPensamento() {
+    console.log(this.formulario.get('autoria')?.errors)
+    if (this.formulario.valid){
+      this.service.criar(this.formulario.value).subscribe(()=>{
+        this.router.navigate(['/listarPensamento'])
+      })
+    }
   }
   cancelarPensamento(){
     this.router.navigate(['/listarPensamento'])
+  }
+
+  habilitarBotao(): string {
+    if(this.formulario.valid){
+      return 'botao'
+    } else {
+      return 'botao__desabilitado'
+    }
   }
 }
